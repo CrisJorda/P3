bed93b4129466d64e45ecb21ba7364a67485912c
Laura Rayón Ropero <laurayon99@gmail.com>
2021-04-25 16:37:22 +0200

    Implementation of Center Clipping, update on scripts and docopt

--- scripts/optimize_vad.sh
+++ scripts/optimize_vad.sh
@@ -11,16 +11,17 @@ rm temp.txt
 for d in $(seq 0.475 0.475); do
     for p in $(seq 6 6); do
         for r in $(seq 0 0); do
-            for fwav in pitch_db/train/*.wav; do
-                ff0=${fwav/.wav/.f0}
-                echo "$GETF0 $fwav $ff0 -r $r -p $p -d $d"
-                $GETF0 $fwav $ff0 -r $r -p $p -d $d > /dev/null || (echo "Error in $GETF0 $fwav $ff0"; exit 1)
-            done
+            for c in $(seq 0.00000 0.00001 0.0001); do
+                for fwav in pitch_db/train/*.wav; do
+                    ff0=${fwav/.wav/.f0}
+                    echo "$GETF0 $fwav $ff0 -r $r -p $p -d $d"
+                    $GETF0 $fwav $ff0 -r $r -p $p -d $d -c $c > /dev/null || (echo "Error in $GETF0 $fwav $ff0"; exit 1)
+                done
 
-            pitch_evaluate pitch_db/train/*.f0ref | tee ~/PAV/P3/temp.txt
-            echo "$r $p $d" >> results.txt
-            tail -3 temp.txt >> results.txt
-
+                pitch_evaluate pitch_db/train/*.f0ref | tee ~/PAV/P3/temp.txt
+                echo "$r $p $d $c" >> results.txt
+                tail -3 temp.txt >> results.txt
+            done 
         done
     done
 done
